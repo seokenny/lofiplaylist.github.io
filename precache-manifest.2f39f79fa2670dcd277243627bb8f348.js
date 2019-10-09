c9961e0964806d4aa512736349a06fe41e105fde
seokenny <kenn.seo94@gmail.com>
2019-10-09 14:25:56 -0400

    Updates

---
+++ precache-manifest.2f39f79fa2670dcd277243627bb8f348.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea85ad512a46b7cc4b51a039579272f9",
+    "url": "/lofiplaylist.github.io/index.html"
+  },
+  {
+    "revision": "43d9a66ede52733c835b",
+    "url": "/lofiplaylist.github.io/static/css/main.b870a3fa.chunk.css"
+  },
+  {
+    "revision": "ff25fb86ea61a2917712",
+    "url": "/lofiplaylist.github.io/static/js/2.a142b963.chunk.js"
+  },
+  {
+    "revision": "43d9a66ede52733c835b",
+    "url": "/lofiplaylist.github.io/static/js/main.f4fff5cc.chunk.js"
+  },
+  {
+    "revision": "96829d6a1a0f373a59fe",
+    "url": "/lofiplaylist.github.io/static/js/runtime-main.a3911785.js"
+  }
+]);
